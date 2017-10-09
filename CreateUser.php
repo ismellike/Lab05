bdf135a3860c6742ef2fb575b84f3ef335884324
Gabriel Lopez <g137l275@ku.edu>
2017-10-09 14:00:35 -0500

    testing exercise 2

--- CreateUser.php
+++ CreateUser.php
@@ -1,7 +1,9 @@
 <?php
+//establish connections
 $users = new mysqli("mysql.eecs.ku.edu","glopez", "P@$$word123", "Users");
 $posts = new mysqli("mysql.eecs.ku.edu","glopez", "P@$$word123", "Posts");
 
+//check connections
 if($users->connect_errno) 
 {
 	printf("Connect failed: %s\n", $users->connect_errno);
@@ -13,5 +15,23 @@ if($posts->connect_errno)
 	exit();
 }
 
+//read input
+$user = $_POST["user"];
+if($user == "")
+{
+	echo "The user field cannot be left empty.";
+	exit();
+}
 
+if($result = $users->query("SELECT Users FROM user_id"))
+{
+	while($row = $results->fetch_assoc())
+	{
+		echo $row["user_id"];
+	}
+	$results->free();
+}
+
+$users->close();
+$posts->close();
 ?>
