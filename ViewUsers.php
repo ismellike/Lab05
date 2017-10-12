9b04c449cab02a6da9e92f8eb107752fe511ef09
Gabriel Lopez <g137l275@ku.edu>
2017-10-12 13:59:43 -0500

    Finished ViewUsers.php

---
+++ ViewUsers.php
@@ -0,0 +1,25 @@
+<?php
+//establish connections
+$mysql = new mysqli("mysql.eecs.ku.edu","glopez", 'P@$$word123', "glopez");
+
+//check connection
+if($mysql->connect_errno) 
+{
+	printf("Connect failed to mysql.");
+	exit();
+}
+
+//check if person exists
+if($result = $mysql->query("SELECT * FROM Users"))
+{
+	echo "<table>";
+	echo "<tr><th>user_id</th></tr>";
+	while($row = $result->fetch_assoc())
+	{
+	echo "<tr><td>".$row["user_id"]."</td></tr>";
+	}
+	echo"</table>";
+	$result->free();
+}
+$mysql->close();
+?>
